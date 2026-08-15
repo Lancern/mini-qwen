6ff6c53bbcf138a6e602c8ed8a4c9a37498d3fcd
Sirui Mu <msrlancern@gmail.com>
2026-08-15 18:45:18 +0800

    feat: add TTFT and TPOT measurement

--- repl.py
+++ repl.py
@@ -3,7 +3,7 @@
 import os
 from argparse import ArgumentParser
 from os import PathLike
-from time import time
+from time import monotonic
 from typing import cast
 
 import torch
@@ -13,19 +13,28 @@ from miniqwen.model import MiniQwen
 
 def sample_response(model: MiniQwen, prompt: str, max_generate_len: int = 1000):
     num_tokens_generated = 0
-    start_time = time()
+    start_time = monotonic()
+    first_token_time: float | None = None
 
     for token in model.generate(prompt, max_generate_len=max_generate_len):
+        if first_token_time is None:
+            first_token_time = monotonic()
         print(token, end="", flush=True)
         num_tokens_generated += 1
 
-    end_time = time()
+    end_time = monotonic()
     print()
 
     elapsed_sec = end_time - start_time
-    tokens_per_sec = num_tokens_generated / elapsed_sec if elapsed_sec > 0 else 0.0
+    tokens_per_sec = num_tokens_generated / elapsed_sec
     print(f"--- {num_tokens_generated} token(s) generated in {elapsed_sec:.2f} seconds")
     print(f"--- {tokens_per_sec:.2f} token(s)/sec")
+
+    if first_token_time is not None:
+        ttft = first_token_time - start_time
+        tpot = (end_time - first_token_time) / (num_tokens_generated - 1)
+        print(f"--- TTFT: {ttft} sec")
+        print(f"--- TPOT: {tpot * 1000} ms")
 
     if cache := model.model.kv_cache:
         print(f"--- KV cache size: {cache.size} byte(s)")
